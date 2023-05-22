187e755ee2805e7786da8985b36a69bef29e2be1
kuviman <kuviman@gmail.com>
2023-05-22 16:30:42 +0400

    dragging/clicking

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
 use geng::prelude::*;
 
+enum Drag {
+    None,
+    Detecting { from: vec2<f32>, timer: Timer },
+    Dragging { prev_mouse_pos: vec2<f32> },
+}
+
+#[derive(Deserialize)]
+struct Config {
+    pub min_drag_distance: f32,
+}
+
 struct Game {
     geng: Geng,
+    framebuffer_size: vec2<f32>,
     camera: geng::Camera2d,
+    drag: Drag,
+    config: Config,
 }
 
 impl Game {
-    pub fn new(geng: &Geng) -> Self {
+    pub fn new(geng: &Geng, config: Config) -> Self {
         Self {
+            config,
             geng: geng.clone(),
+            framebuffer_size: vec2::splat(1.0),
             camera: geng::Camera2d {
                 center: vec2::ZERO,
                 rotation: 0.0,
                 fov: 10.0,
             },
+            drag: Drag::None,
         }
     }
 }
 
 impl geng::State for Game {
     fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
+        self.framebuffer_size = framebuffer.size().map(|x| x as f32);
         ugli::clear(framebuffer, Some(Rgba::BLACK), None, None);
         const N: i32 = 10;
         for i in -N..=N {
@@ -43,9 +61,56 @@ impl geng::State for Game {
             );
         }
     }
+    fn handle_event(&mut self, event: geng::Event) {
+        let world_pos = |screen_pos| {
+            self.camera
+                .screen_to_world(self.framebuffer_size, screen_pos)
+        };
+        match event {
+            geng::Event::MouseDown { position, .. }
+            | geng::Event::TouchStart(geng::Touch { position, .. }) => {
+                let pos = position.map(|x| x as f32);
+                self.drag = Drag::Detecting {
+                    from: pos,
+                    timer: Timer::new(),
+                };
+            }
+            geng::Event::MouseMove { position, .. }
+            | geng::Event::TouchMove(geng::Touch { position, .. }) => {
+                let pos = position.map(|x| x as f32);
+                if let Drag::Detecting { from, .. } = self.drag {
+                    if (from - pos).len() > self.config.min_drag_distance {
+                        self.drag = Drag::Dragging {
+                            prev_mouse_pos: from,
+                        };
+                    }
+                }
+                if let Drag::Dragging { prev_mouse_pos } = &mut self.drag {
+                    self.camera.center += world_pos(*prev_mouse_pos) - world_pos(pos);
+                    *prev_mouse_pos = pos;
+                }
+            }
+            geng::Event::MouseUp { position, .. }
+            | geng::Event::TouchEnd(geng::Touch { position, .. }) => {
+                let pos = position.map(|x| x as f32);
+                if let Drag::Detecting { .. } = self.drag {
+                    log::info!("Clicked at {pos:?}");
+                }
+                self.drag = Drag::None;
+            }
+            _ => {}
+        }
+    }
 }
 
 fn main() {
+    logger::init();
+    geng::setup_panic_handler();
     let geng = Geng::new("Find Ferris");
-    geng.clone().run_loading(async move { Game::new(&geng) });
+    geng.clone().run_loading(async move {
+        let config = file::load_detect(run_dir().join("assets").join("config.toml"))
+            .await
+            .unwrap();
+        Game::new(&geng, config)
+    });
 }
