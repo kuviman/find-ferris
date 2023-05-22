ee38a38cbfacec7a2a03f1290bbd8c7b675e8ac3
kuviman <kuviman@gmail.com>
2023-05-22 15:57:10 +0400

    Draw grid

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,18 @@ use geng::prelude::*;
 
 struct Game {
     geng: Geng,
+    camera: geng::Camera2d,
 }
 
 impl Game {
     pub fn new(geng: &Geng) -> Self {
         Self {
             geng: geng.clone(),
+            camera: geng::Camera2d {
+                center: vec2::ZERO,
+                rotation: 0.0,
+                fov: 10.0,
+            },
         }
     }
 }
@@ -15,6 +21,27 @@ impl Game {
 impl geng::State for Game {
     fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
         ugli::clear(framebuffer, Some(Rgba::BLACK), None, None);
+        const N: i32 = 10;
+        for i in -N..=N {
+            self.geng.draw2d().draw2d(
+                framebuffer,
+                &self.camera,
+                &draw2d::Segment::new(
+                    Segment(vec2(i as f32, -N as f32), vec2(i as f32, N as f32)),
+                    0.1,
+                    Rgba::GRAY,
+                ),
+            );
+            self.geng.draw2d().draw2d(
+                framebuffer,
+                &self.camera,
+                &draw2d::Segment::new(
+                    Segment(vec2(-N as f32, i as f32), vec2(N as f32, i as f32)),
+                    0.1,
+                    Rgba::GRAY,
+                ),
+            );
+        }
     }
 }
 
